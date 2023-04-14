517da901d5c0cf9b772ec520af2d477b112dbeda
Agrawal, Mana <manaagra@iu.edu>
2023-04-14 13:54:34 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 from gmaze import cluster
 import numpy as np
+from utils import load_graph, get_embedding, deepwalk_walks
+
+
+
+
+
+# walks_deepwalk = deepwalk_walks(G, walk_length=l, num_walks=80)
+# embeddings_deepwalk = get_embedding(G,walks_deepwalk)
+# modularity_scores = cluster_eval(G1, embeddings_deepwalk)
 
 points = np.array([ [9.0, 10.0], [1000, 9000],[1,2],[2,1],[2,2],[9,9],[1000,8000]])
 k = 3

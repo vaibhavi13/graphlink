28b00bea6e01a9c0e225df9bb01f6774205acc2f
Vaibhavi <vakpatel@iu.edu>
2023-04-22 13:47:43 -0400

    refactored code for embedding

--- main.py
+++ main.py
@@ -5,16 +5,14 @@ import community as comm
 from sklearn.cluster import KMeans
 import time
 
-
-
 G = load_graph()
 l = 1
 walks_deepwalk = deepwalk_walks(G, walk_length=l, num_walks=80)
 embeddings_deepwalk = get_embedding(G,walks_deepwalk)
 G1 = G.to_undirected()
 
-points = [[0] for i in range(G1.number_of_nodes())]
-for i in range(0, 4038):
+points = [[0] for i in range(G1.number_of_nodes()-1)]
+for i in range(0, G1.number_of_nodes()-1):
     points[i] = embeddings_deepwalk[str(i+1)]
 points = np.array(points)
 
@@ -25,28 +23,16 @@ print("shape of points is "+ str(points.shape))
 #k = 3
 
 max_iters = 10
-NOC = 10
+NOC = 100
 
 start_time = time.time()
-print("\nStarting kmeans: ")
-print("\n start time is "+ str(start_time))
+print("\nStarting kmeans...")
 
-for k in range(9, NOC):
+for k in range(99,NOC):
     clusters = cluster.kmeans(points, k, max_iters)
-    predG = dict()
-    for node in range(len(clusters.labels_)):
-        predG[node] = clusters.labels_[node]
-    
-    # compute the modularity score of the Kmeans clustering
-    modularity = comm.community_louvain.modularity(predG, G)
-    allmodularity.append(modularity)
-    print("Number of clusters: ", k, "  Modularity: ", modularity)
 
-#print(cluster.kmeans(points, k, max_iters))
-
+print("\nFinished kmeans...")
 end_time = time.time()
 
 elapsed_time = end_time - start_time
-
 print("Execution time:", elapsed_time, "seconds")
-
